925546b1280d194e0cfec8df23a0b68cc5fc1adb
Misterlinux <mrlzarate8@gmail.com>
2025-02-13 18:11:55 +0100

    all option

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ const PORT = process.env.PORT || 3020;
 /*const session = require('express-session');*/
 
 const cors = require("cors");
-
+/*
 const corsOptions = {
   origin: "http://localhost:3000", 
   methods: [`DELETE`,  `PUT`, `POST`],
   allowedHeaders: ["Content-Type"],
   credentials: true
 };
+*/
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use( cors(corsOptions) )
 
