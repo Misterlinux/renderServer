23aea1b10778715189e922d5f5ed696b8d89a043
Misterlinux <mrlzarate8@gmail.com>
2025-02-13 18:08:38 +0100

    second change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: "http://localhost:3000", 
-  methods: [`DELETE`,  `PUT`],
+  methods: [`DELETE`,  `PUT`, `POST`],
   allowedHeaders: ["Content-Type"],
   credentials: true
 };
