7507659631ff655030f7b59822f6c13fe53e27e0
Misterlinux <mrlzarate8@gmail.com>
2025-02-14 16:22:15 +0100

    cors option way

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type"],
   credentials: true
 };
-
+/*
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -29,9 +29,9 @@ app.all('*', function(req, res, next) {
     next();
   }
 });
+*/
 
-
-//app.use( cors(corsOptions) )
+app.use( cors(corsOptions) )
 
 app.use(express.json())
 
